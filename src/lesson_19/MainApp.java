1f364d54ba199c68185d76b06f4ced2d227536f1
Elena Karsten <e.karstenzv@gmail.com>
2024-10-08 15:29:53 +0200

    hw_19 add

---
+++ src/lesson_19/MainApp.java
@@ -0,0 +1,10 @@
+package lesson_19;
+
+public class MainApp {
+    public static void main(String[] args) {
+        FinalDemo finalDemo = new FinalDemo(50);
+
+        System.out.println(FinalDemo.PI);
+        System.out.println(FinalDemo.COUNTRY);
+    }
+}
